481d8eed541594bcd6aeda2c64024277b11ccaf5
Jun Clemente <kjstudios858@gmail.com>
2018-05-02 21:28:35 +0000

    refactor Update files, create folders

--- flask_app.py
+++ flask_app.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Jun @ www.codeandlight.com!<br><p>Site coming soon...</p>'
 
+
+if __name__ == "__main__":
+    app.run(debug=True,
+            host='0.0.0.0', port=8080)
