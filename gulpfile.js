4d8c78cb573975760b1993d6406a06d9dbe02b83
Jun C <kjstudios858@gmail.com>
2018-08-19 16:35:47 -0700

    chore Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 // gulpjs configuration file
 
+'use strict';
+
 var gulp = require('gulp'),
     newer = require('gulp-newer'),
     del = require('del'),
@@ -7,42 +9,38 @@ var gulp = require('gulp'),
     cleancss = require('gulp-clean-css'),
     pleeease = require('gulp-pleeease'),
     urlAdjuster = require('gulp-css-url-adjuster'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    htmlclean = require('gulp-htmlclean'),
     concat = require('gulp-concat'),
-    // gulpCopy = require('gulp-copy'),
     size = require('gulp-size'),
     imagemin = require('gulp-imagemin');
 
 
-// file locations
 var source = 'source/',
     dest = 'flaskapp/',
 
-    // define source/destination files for html
     html = {
         in: source + 'templates/*.html',
-        out: dest + 'templates',
-        options: { prefix: 1 }
+        out: dest + 'templates/'
     },
 
     js = {
-        in: source + 'static/js/*.*',
-        out: dest + 'static/js/',
-        options: { prefix: 1 }
+        in: source + 'static/js/*.js',
+        out: dest + 'static/js/'
     },
 
     images = {
-        in: source + 'static/images/*.*',
+        in: source + 'static/images/**/*.*',
         out: dest + 'static/images/'
     },
 
     css = {
-        in: source + 'static/css/*.*',
+        in: source + 'static/css/*.css',
         out: dest + 'static/css/',
         pleeeaseOpts: {
             autoprefixer: { browsers: ['last 2 version', '>5%' ]},
-            // pseudoElements: true,
             mqpacker: true,
-            rem: ['16px'],
             minifier: false
         }
     },
@@ -56,7 +54,6 @@ var source = 'source/',
 
 // Define Tasks
 gulp.task('clean', function () {
-    "use strict";
     del([dest + '*']).then(paths => {
         console.log('Deleted files and folders:\n',
                     paths.join('\n'));
@@ -65,43 +62,33 @@ gulp.task('clean', function () {
 
 
 gulp.task('python', function() {
-    "use strict";
-    // return gulp.src(pyFiles.in)
-    //     .pipe(newer(dest))
-    //     .pipe(gulpCopy(dest, pyFiles.options))
-    //     .pipe(gulp.dest(dest));
     return gulp.src(pyFiles.in)
         .pipe(newer(dest))
         .pipe(gulp.dest(dest));
 });
 
 gulp.task('html', function () {
-    "use strict";
     return gulp.src(html.in)
         .pipe(newer(html.out))
-        // .pipe(gulpCopy(dest, html.options))
+        .pipe(size({title: 'HTML in: '}))
+        .pipe(htmlclean())
+        .pipe(size({title: 'HTML out: '}))
         .pipe(gulp.dest(html.out))
 });
 
 gulp.task('js', function () {
-    "use strict";
     return gulp.src(js.in)
         .pipe(newer(js.out))
         .pipe(size({title: 'JavaScript in: '}))
-        // .pipe(gulpCopy(dest, js.options))
+        .pipe(uglify())
         .pipe(size({title: 'JavaScript out: '}))
         .pipe(gulp.dest(js.out))
 });
 
 gulp.task('css', function () {
-    "use strict";
     return gulp.src(css.in)
         .pipe(concat('styles.css'))
         .pipe(cssImport())
-    //     .pipe(cleancss())
-    //     .pipe(gulp.dest(css.out));
-
-    // return gulp.src(css.in)
         .pipe(size({title: 'CSS in: '}))
         .pipe(pleeease(css.pleeeaseOpts))
         .pipe(size({title: 'CSS out: '}))
@@ -109,7 +96,6 @@ gulp.task('css', function () {
 });
 
 gulp.task('images', function () {
-    "use strict";
     return gulp.src(images.in)
         .pipe(newer(images.out))
         .pipe(size({title: 'Image In: '}))
