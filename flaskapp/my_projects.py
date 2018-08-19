b0aa7aedf1edb3f8139fb7dea4f76f06624bb7c0
Jun C <kjstudios858@gmail.com>
2018-08-19 16:39:00 -0700

    chore Build production code

---
+++ flaskapp/my_projects.py
@@ -0,0 +1,24 @@
+projects = [
+        {
+        "title": "GS Communication Website",
+        "url": "http://www.gs-communication.com/",
+        "code": "https://github.com/junclemente/flask_gscom",
+        "image": "gscom.jpeg",
+        "desc": "Bacon ipsum dolor amet ground round doner venison jerky shankle beef ribs. Landjaeger shoulder biltong t-bone pork belly pastrami. Kielbasa andouille pork belly, boudin meatloaf frankfurter jerky kevin t-bone filet mignon. Biltong picanha jerky landjaeger. Buffalo filet mignon beef meatball beef ribs boudin."
+        },
+        {
+        "title": "Peptide Sequence Conversion Tool",
+        "url": "",
+        "code": "https://github.com/junclemente/peptools",
+        "image": "peptidetool.jpeg",
+        "desc": "Bacon ipsum dolor amet ground round doner venison jerky shankle beef ribs. Landjaeger shoulder biltong t-bone pork belly pastrami. Kielbasa andouille pork belly, boudin meatloaf frankfurter jerky kevin t-bone filet mignon. Biltong picanha jerky landjaeger. Buffalo filet mignon beef meatball beef ribs boudin."
+
+        },
+        {
+        "title": "Wedding Website",
+        "url": "http://www.graceandmelvin.com",
+        "code": "https://github.com/junclemente/mgsite",
+        "image": "mgsite.jpg",
+        "desc": "Bacon ipsum dolor amet ground round doner venison jerky shankle beef ribs. Landjaeger shoulder biltong t-bone pork belly pastrami. Kielbasa andouille pork belly, boudin meatloaf frankfurter jerky kevin t-bone filet mignon. Biltong picanha jerky landjaeger. Buffalo filet mignon beef meatball beef ribs boudin."
+        }
+]
