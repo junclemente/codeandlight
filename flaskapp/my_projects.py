f872c659efcfcf543bec5010bc87e7f2ddf92f09
Jun C <kjstudios858@gmail.com>
2018-08-19 19:05:50 -0700

    chore Build update

--- flaskapp/my_projects.py
+++ flaskapp/my_projects.py
@@ -3,22 +3,62 @@ projects = [
         "title": "GS Communication Website",
         "url": "http://www.gs-communication.com/",
         "code": "https://github.com/junclemente/flask_gscom",
+        "icons": ["html5", "js-square", "python", "css3-alt"],
         "image": "gscom.jpeg",
-        "desc": "Bacon ipsum dolor amet ground round doner venison jerky shankle beef ribs. Landjaeger shoulder biltong t-bone pork belly pastrami. Kielbasa andouille pork belly, boudin meatloaf frankfurter jerky kevin t-bone filet mignon. Biltong picanha jerky landjaeger. Buffalo filet mignon beef meatball beef ribs boudin."
+        "desc": "Designed and developed this website for the engineering firm \
+        GS Communication, giving them their first web presence. The backend is \
+        developed with Python, Flask, and Jinja2. The frontend is designed \
+        using HTML5, CSS3 and Bootstrap."
         },
         {
         "title": "Peptide Sequence Conversion Tool",
         "url": "",
         "code": "https://github.com/junclemente/peptools",
+        "icons": ["python", "html5", "css3-alt", "js-square"],
         "image": "peptidetool.jpeg",
-        "desc": "Bacon ipsum dolor amet ground round doner venison jerky shankle beef ribs. Landjaeger shoulder biltong t-bone pork belly pastrami. Kielbasa andouille pork belly, boudin meatloaf frankfurter jerky kevin t-bone filet mignon. Biltong picanha jerky landjaeger. Buffalo filet mignon beef meatball beef ribs boudin."
-
+        "desc": "This is a simple web app using Flask and Python. It takes \
+        a 1-letter amino acid chain representation and converts it to the \
+        3-letter representation. I developed this web app to improve the \
+        efficiency of the sales team and chemists at a company I was \
+        previously working for."
         },
         {
         "title": "Wedding Website",
         "url": "http://www.graceandmelvin.com",
         "code": "https://github.com/junclemente/mgsite",
+        "icons": ["html5", "css3-alt", "js-square"],
         "image": "mgsite.jpg",
-        "desc": "Bacon ipsum dolor amet ground round doner venison jerky shankle beef ribs. Landjaeger shoulder biltong t-bone pork belly pastrami. Kielbasa andouille pork belly, boudin meatloaf frankfurter jerky kevin t-bone filet mignon. Biltong picanha jerky landjaeger. Buffalo filet mignon beef meatball beef ribs boudin."
+        "desc": "I designed and developed this responsive website for the \
+        wedding of my brother and his fiance. The site incorporates Bootstrap, \
+        HTML5, CSS3, JavaScript, jQuery and KnockoutJS. It is a single page \
+        application (SPA). To minimize the costs for the bride and groom, the \
+        site is hosted on Github Pages using a custom domain name from GoDaddy. \
+        The RSVP is handled using Google Docs services."
+        },
+        {
+        "title": "Body Evolution",
+        "url": "https://www.bodyevolution.org",
+        "code": "",
+        "icons": ["html5", "wordpress", "css3-alt", "php"],
+        "image": "be-jumbotron.jpg",
+        "desc": "I am the currently the manager of this website. When I took \
+        over, I did not know WordPress or PHP but I am gradually learning more \
+        about this CMS."
+        },
+        {
+        "title": "Catechism Class",
+        "url": "https://www.catechismclass.com",
+        "code": "",
+        "icons": ["html5", "css3-alt", "php", "js-square"],
+        "image": "cat-class-shield.png",
+        "desc": "I am a remote volunteer for this website. I develop "
         }
+        # {
+        # "title": "",
+        # "url": "",
+        # "code": "",
+        # "icons": [],
+        # "image": "",
+        # "desc": ""
+        # }
 ]
